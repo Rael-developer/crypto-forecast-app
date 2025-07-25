296f4d9cc8c3c3bd9b4f358acf180b5d7f1d7e5d
Israel Rodrigues <israelrodrigues2694@outlook.pt>
2025-07-24 23:15:16 -0300

    Update binance_utils.py

--- binance_utils.py
+++ binance_utils.py
@@ -27,11 +27,11 @@ def get_all_symbols():
         r.raise_for_status()
         data = r.json()
         if "symbols" not in data:
-            return ["BTC/USDT", "ETH/USDT", "BNB/USDT"]  # fallback
+            return ["BTCUSDT", "ETHUSDT", "BNBUSDT"]  # fallback
         symbols = [s['symbol'] for s in data['symbols'] if s['quoteAsset'] == 'USDT']
         return symbols
     except Exception as e:
-        return ["BTC/USDT", "ETH/USDT", "BNB/USDT"]  # fallback se der erro
+        return ["BTCUSDT", "ETHUSDT", "BNBUSDT"]  # fallback se der erro
         
 def get_historical_data(symbol="BTCUSDT", interval="1d", limit=500):
     url = f"{BASE_URL}/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}"
